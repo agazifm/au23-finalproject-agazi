c444a3e8305fe66d47237686dd980b75baccdf93
agazifm <147117677+agazifm@users.noreply.github.com>
2023-12-11 13:38:09 -0800

    Create eventListeners.js
    Event Listeners for capture User Interactions

---
+++ eventListeners.js
@@ -0,0 +1,9 @@
+// Event listener for keyboard inputs (e.g., arrow keys for navigation)
+document.addEventListener('keydown', (event) => {
+    //to handle keyboard inputs
+});
+
+// Event listener for mouse clicks (e.g., to zoom in on an artwork or view additional information)
+document.addEventListener('click', (event) => {
+    //to handle mouse clicks
+});
